e09632504bbe691c5634bd2c483603e2e565f539
Oliver Peña <oliverdpz98@gmail.com>
2022-01-27 12:15:57 -0600

    Primera prueba

---
+++ Primera/Prueba/Test1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name;
+    
+    cout <<"ingrese su nombre"<<endl; 
+    cin >> name;
+
+    cout <<"Su nombre es: " << name <<endl << "Fin del programa" <<endl;
+
+
+    return 0;
+}
