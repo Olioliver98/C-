49d825083f317a87da12e9445cf697b92f7a0b31
Oliver Peña <oliverdpz98@gmail.com>
2022-01-27 13:13:29 -0600

    Actualizacion

--- Ejercicios/Ejercicio_2/Ejercicio2.cpp
+++ Ejercicios/Ejercicio_2/Ejercicio2.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 int main() {
 
+    // Se declara la variable en tipo caracter
     string name;
     
+    //Muestra en pantalla una peticion de nombre
     cout <<"ingrese su nombre"<<endl; 
+
+    // Se almacena la variable
     cin >> name;
 
+    //Se muestra la variable junto con un salto de linea y el mennsaje Fin del programa.
     cout <<"Su nombre es: " << name <<endl << "Fin del programa" <<endl;
 
 
