674d702083aefbf601430e859bb8fe8e0923af9b
Oliver Peña <oliverdpz98@gmail.com>
2022-01-28 12:22:24 -0600

    Ejercicio 4

---
+++ Ejercicios/Ejercicio_4/Todas-Las-Variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool ValorBoolean;
+    ValorBoolean = false;
+
+    int ValorEntero = 15;
+    double ValorDouble = 20.99;
+    string ValorSting = "Hola como estan";
+    char ValorChar = 'B';
+    
+
+    cout << "Valor Boolean: " << ValorBoolean << endl;
+    cout << "Valor Entero: " << ValorEntero << endl;
+    cout << "Valor Double: " << ValorDouble << endl;
+    cout << "Valor Sting: " << ValorSting << endl;
+    cout << "Valor Char: " << ValorChar << endl;
+
+    
+ return 0;
+}
