3b819ae492c16e24c64dd1141edca5cd87bb4a6d
Oliver Peña <oliverdpz98@gmail.com>
2022-01-28 12:53:47 -0600

    actualizacion

---
+++ Ejercicios/Ejercicio_ 5 ProgramaSuma/ProgramaSuma.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // Datos de entrada
+    int a = 0, b = 0, resultado = 0;
+
+    cout <<"Ingrese el valor de A";
+    cin >> a;
+
+    cout <<endl;
+
+    cout <<"Ingrese el valor de B";
+    cin >> b;
+
+    cout <<endl;
+
+    //Proceso
+
+    resultado = a + b;
+    cout << "La suma de A + B es: " << resultado;
+  
+    return 0;
+}
+
+
+
+
+
