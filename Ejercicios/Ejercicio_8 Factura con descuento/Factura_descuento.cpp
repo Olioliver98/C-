367c7f99061cbbe87a6d96ca8ba5cc45cf50b259
Oliver Peña <oliverdpz98@gmail.com>
2022-02-05 12:46:31 -0600

    factura con descuento

--- Ejercicios/Ejercicio_8 Factura con descuento/Factura_descuento.cpp
+++ Ejercicios/Ejercicio_8 Factura con descuento/Factura_descuento.cpp
@@ -3,9 +3,30 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    // Datos de entrada
+    double subtotal = 0, total = 0, Impuesto = 0.15, calculo_descuento = 0, calculo_impuesto = 0;
+    int descuento = 0;
+
+    cout<< "Ingrese el subtotal de la factura:  ";
+    cin>> subtotal;
+
+    cout<<endl;
+ 
+    cout<<"Ingrese el descuento (0, 10, 20):  ";
+    cin>> descuento;
+
+    cout<<endl;
+
+    // Proceso
+    calculo_descuento = (subtotal * descuento) / 100;
+    calculo_impuesto = (subtotal - calculo_descuento) * 0.15;
+    total = subtotal - calculo_descuento + calculo_impuesto;
+
+    // Salida
+    cout<<endl;
+    cout<< "Total a pagar es:  " << total;
 
 
-    
+
     return 0;
 }
