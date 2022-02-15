acfe85482d7d264b5d661f120819980e1f647e0a
Oliver Peña <oliverdpz98@gmail.com>
2022-02-15 11:42:28 -0600

    Agregando ejercicio 16

---
+++ Ejercicios/Ejercicio_16 Ciclo while/while.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 5, b = 2;
+
+
+    while (a > b)
+    {
+        cout<<"Ingrese el valor de A"<<endl;
+        cin>>a;
+
+        
+    }
+    
+    cout<<endl;
+    cout<<"Saliste del ciclo! :D";
+
+    return 0;
+}
