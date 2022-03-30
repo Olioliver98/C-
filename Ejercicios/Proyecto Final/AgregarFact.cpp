053fa2943df0381284fea1c714c4db5be4668c43
Oliver Peña <oliverdpz98@gmail.com>
2022-03-30 12:02:14 -0600

    En proyecto

---
+++ Ejercicios/Proyecto Final/AgregarFact.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void AgregarFact()
+{
+
+    cout <<"        Agregar un producto     "<<endl;
+    cout <<"                                "<<endl;
+    
+
+
+
+
+
+
+}
---
+++ Ejercicios/Proyecto Final/Facturacion.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include "AgregarFact.h"
+
+using namespace std;
+
+void facturacion()
+{
+    int opcion;
+
+    system("cls");
+    cout << "       Facturacion         "<<endl;
+    cout << "                           "<<endl;
+    cout << "1. Agregar factura         "<<endl;
+    cout << "2. Agegar factura 3ra edad "<<endl;
+    cout << "3. Cotizar producto        "<<endl;
+    cin >> opcion;
+
+    switch (opcion)
+    {
+    case 1:
+    {
+        
+    }
+
+        break;
+    
+    default:
+        break;
+    }
+
+
+
+
+
+
+
+
+}
